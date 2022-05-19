4d3a8e9b056e4655b566506ff37ee5e6dd9f8f83
πόσο σ' αγαπώ <1192338674@qq.com>
2022-05-19 18:38:22 +0800

    Create pre_train_encoder.py
    prepare for code reformat

---
+++ pnmt/encoders/pre_train_encoder.py
@@ -0,0 +1,13 @@
+from pnmt.encoders.encoder import EncoderBase
+
+
+class PreTrainEncoder(EncoderBase):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def from_opt(cls, opt, embeddings=None):
+        pass
+    
